63239ae13d0cd21963a504fdc5b9c37dac4ae5d9
Luke Wallin <luke@lukewallin.co.uk>
2020-04-15 19:50:15 +0100

    fixed auto versioning

--- build.sh
+++ build.sh
@@ -1,29 +1,29 @@
 #! /bin/bash
 
-rm -r out || true
+rm -r build || true
 
-mkdir -p out/var/www/
+mkdir -p build/var/www/
 pushd train-control
 ng build
-mv dist/train-control ../out/var/www/trains
+mv dist/train-control ../build/var/www/trains
 popd
-cp -r DEBIAN out/
-mkdir -p out/etc/systemd/system
-mkdir -p out/etc/nginx/sites-enabled
+cp -r DEBIAN build/
+mkdir -p build/etc/systemd/system
+mkdir -p build/etc/nginx/sites-enabled
 
-cp config/trains.conf out/etc/nginx/sites-enabled/trains.conf
-cp config/trains.service out/etc/systemd/system/trains.service
+cp config/trains.conf build/etc/nginx/sites-enabled/trains.conf
+cp config/trains.service build/etc/systemd/system/trains.service
 
-#skip out anything non-python
-mkdir -p out/usr/share/trains/trains
-cp -r train-control-server/*.py out/usr/share/trains/
-cp -r train-control-server/trains/*.py out/usr/share/trains/trains/
+#skip build anything non-python
+mkdir -p build/usr/share/trains/trains
+cp -r train-control-server/*.py build/usr/share/trains/
+cp -r train-control-server/trains/*.py build/usr/share/trains/trains/
 
 
-mkdir build || true
+mkdir out || true
 
 VERSION="$(git describe)"
 
-sed -i 's/VERSION/${VERSION}/g' out/DEBIAN/control
+sed -i "s/VERSIONHERE/${VERSION}/g" build/DEBIAN/control
 
-fakeroot dpkg-deb -b out build
+fakeroot dpkg-deb -b build out
